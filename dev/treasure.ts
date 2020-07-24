5df80cf76385781f01b336a05ecef9bc4d6bd8e6
Lucas Hijman <46541848+0968128@users.noreply.github.com>
2020-07-24 14:24:37 +0200

    update treasure
    Meer comments voor verduidelijking

--- dev/treasure.ts
+++ dev/treasure.ts
@@ -13,15 +13,22 @@ class Treasure extends GameObject {
         let game = document.getElementsByTagName("game")[0]
         game.appendChild(this)
 
+        // Locatie in het scherm is willekeurig 
         this.x = Math.random() * window.innerWidth
         this.y = Math.random() * window.innerHeight
     }
 
-    public onCollision(gameObject: GameObject): void {
+    // Functionaliteit bij een botsing met een gegeven gameobject
+    public onCollision(gameObject:GameObject):void {
         if(gameObject instanceof PlayerShip) {
+            // Punt optellen bij score
             Game.getInstance().addScore()
+            
+            // Piratenschepen moeten varen naar waar de vorige schat was, dus die waardes sla ik op in een aparte variabele
             this._previousChestX = this.x
             this._previousChestY = this.y
+            
+            // De locatie van de schat verandert naar een willekeurige andere plek
             this.x = Math.random() * window.innerWidth
             this.y = Math.random() * window.innerHeight
         }
